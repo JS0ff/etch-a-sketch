d9e1803942a2ebd62c0cec732023ef47d3234695
Asan <onlylearningjs@gmail.com>
2025-03-28 00:25:17 +0600

    Create a function to produce div container

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const mainContainer = document.querySelector(".container");
+
+function createGrid() {
+  let gridDivs = document.createElement("div");
+  gridDivs.className = "grid";
+  gridDivs.textContent = "<>";
+  row.appendChild(gridDivs);
+  row.innerHTML += "\n";
+}
