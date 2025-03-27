3bc6bb29a231866c33312080f8242804fafe9eca
Asan <onlylearningjs@gmail.com>
2025-03-28 00:27:38 +0600

    Create a loop to produce 16x16 grid

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ function createGrid() {
   row.appendChild(gridDivs);
   row.innerHTML += "\n";
 }
+
+for (let i = 1; i <= 16; i++) {
+  row = document.createElement("div");
+  row.className = "row";
+  mainContainer.appendChild(row);
+  for (let j = 1; j <= 16; j++) {
+    sum += 1;
+    createGrid();
+  }
+}
